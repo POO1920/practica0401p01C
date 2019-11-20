dfa7ccf30b943baa2ef0bd7ba8aca30b993374a4
joheras <joheras@gmail.com>
2019-11-20 15:41:48 +0100

    Initial commit

--- Tests.cpp
+++ Tests.cpp
@@ -154,14 +154,14 @@ TEST_CASE("Comprobando constructores y métodos get Matricula", "[Constructores-
 
 
 
-    Matricula * m = new Matricula(a1,e1,C1718,M1,10);
+    Matricula * m = new Matricula(a1,e1,C2122,M1,10);
     REQUIRE( m->getActividad()->getNombre() == "POO" );
     REQUIRE( m->getActividad()->getCreditos() == 60 );
 
     REQUIRE( m->getEstudiante()->getCuasi() == "peperez" );
     REQUIRE( m->getEstudiante()->getFamiliaNumerosa() == true);
     REQUIRE( m->getEstudiante()->getFamiliaNumerosa() == true);
-    REQUIRE( m->getCurso() == C1718);
+    REQUIRE( m->getCurso() == C2122);
     REQUIRE( m->getOrdenMatricula() == M1);
 
 }
@@ -174,8 +174,8 @@ TEST_CASE("Familia no numerosa primera matricula", "[PrimeraMatriculaNoNumerosa]
         Actividad * act1 = new Asignatura("POO", 6, 2010, Troncal);
 		Actividad * act2 = new Reconocimiento("Deportes", 2, "Campeonatos universitarios");
 		Estudiante * est1 = new Estudiante("peperez",false);
-		Matricula * m1 = new Matricula(act1, est1, C1617,M1,20);
-		Matricula * m2 = new Matricula(act2, est1, C1718,M1,10);
+		Matricula * m1 = new Matricula(act1, est1, C1920,M1,20);
+		Matricula * m2 = new Matricula(act2, est1, C2122,M1,10);
 
 		REQUIRE(120 == m1->getPrecio());
 		REQUIRE(15==m2->getPrecio());
@@ -188,8 +188,8 @@ TEST_CASE("Familia no numerosa segunda matricula", "[SegundaMatriculaNoNumerosa]
         Actividad * act1 = new Asignatura("POO", 6, 2010, Troncal);
 		Actividad * act2 = new Reconocimiento("Deportes", 2, "Campeonatos universitarios");
 		Estudiante * est1 = new Estudiante("peperez",false);
-		Matricula * m1 = new Matricula(act1, est1, C1617,M2,20);
-		Matricula * m2 = new Matricula(act2, est1, C1718,M2,10);
+		Matricula * m1 = new Matricula(act1, est1, C1920,M2,20);
+		Matricula * m2 = new Matricula(act2, est1, C2122,M2,10);
 
 		REQUIRE(240 == m1->getPrecio());
 		REQUIRE(30==m2->getPrecio());
@@ -201,8 +201,8 @@ TEST_CASE("Familia no numerosa tercera matricula", "[TerceraMatriculaNoNumerosa]
 Actividad * act1 = new Asignatura("POO", 6, 2010, Troncal);
 		Actividad * act2 = new Reconocimiento("Deportes", 2, "Campeonatos universitarios");
 		Estudiante * est1 = new Estudiante("peperez",false);
-		Matricula * m1 = new Matricula(act1, est1, C1617,M3,20);
-		Matricula  * m2 = new Matricula(act2, est1, C1718,M3,10);
+		Matricula * m1 = new Matricula(act1, est1, C1920,M3,20);
+		Matricula  * m2 = new Matricula(act2, est1, C2122,M3,10);
 
 		REQUIRE(480 == m1->getPrecio());
 		REQUIRE(60==m2->getPrecio());
@@ -213,8 +213,8 @@ TEST_CASE("Familia no numerosa cuarta matricula", "[CuartaMatriculaNoNumerosa]" 
 Actividad * act1 = new Asignatura("POO", 6, 2010, Troncal);
 		Actividad * act2 = new Reconocimiento("Deportes", 2, "Campeonatos universitarios");
 		Estudiante * est1 = new Estudiante("peperez",false);
-		Matricula * m1 = new Matricula(act1, est1, C1617,M4,20);
-		Matricula * m2 = new Matricula(act2, est1, C1718,M4,10);
+		Matricula * m1 = new Matricula(act1, est1, C1920,M4,20);
+		Matricula * m2 = new Matricula(act2, est1, C2122,M4,10);
 
 		REQUIRE(720 == m1->getPrecio());
 		REQUIRE(90==m2->getPrecio());
@@ -226,10 +226,10 @@ Actividad * act1 = new Asignatura("POO", 6, 2010, Troncal);
 		Actividad *  act3 = new Asignatura("TP",6, 2010, Obligatoria);
 		Actividad  * act4 = new Reconocimiento("Deportes", 4, "Campeonatos universitarios");
 		Estudiante  * est1 = new Estudiante("peperez",true);
-		Matricula * m1 = new Matricula(act1, est1, C1617,M1,20);
-		Matricula * m2 = new Matricula(act2, est1, C1718,M2,10);
-		Matricula * m3 = new Matricula(act3, est1, C1617,M3,20);
-		Matricula * m4 = new Matricula(act4, est1, C1718,M4,10);
+		Matricula * m1 = new Matricula(act1, est1, C1920,M1,20);
+		Matricula * m2 = new Matricula(act2, est1, C2122,M2,10);
+		Matricula * m3 = new Matricula(act3, est1, C1920,M3,20);
+		Matricula * m4 = new Matricula(act4, est1, C2122,M4,10);
 
 		REQUIRE(60==m1->getPrecio());
 		REQUIRE(15==m2->getPrecio());
@@ -253,7 +253,7 @@ TEST_CASE("Creacion Matricula", "[testMatriculaCreacion]" ) {
 
         Actividad * act1 = new Asignatura("POO", 6, 2010, Troncal);
 		Estudiante * est1 = new Estudiante("peperez",false);
-		Matricula * m1 = new Matricula(act1, est1, C1617,M3,21.21);
+		Matricula * m1 = new Matricula(act1, est1, C1920,M3,21.21);
 
 		REQUIRE(0==m1->getConvAgotadas());
 
@@ -263,7 +263,7 @@ TEST_CASE("Aniadir Calificacion", "[testAnyadirCalificacion]" ) {
 
         Actividad * act1 = new Asignatura("POO", 6, 2010, Troncal);
 		Estudiante *  est1 = new Estudiante("peperez",false);
-		Matricula * m1 = new Matricula(act1, est1, C1617,M3,21.21);
+		Matricula * m1 = new Matricula(act1, est1, C1920,M3,21.21);
 		Calificacion * c = new Calificacion (20170909, 3);
 		REQUIRE(true==m1->anyadirCalificacion(c));
 		REQUIRE(1==m1->getConvAgotadas());
@@ -279,7 +279,7 @@ TEST_CASE("Aprobados Suspenso", "[testAprobadoSuspenso]" ) {
 
         Actividad * act1 = new Asignatura("POO", 6, 2010, Troncal);
 		Estudiante *  est1 = new Estudiante("peperez",false);
-		Matricula *  m1 = new Matricula(act1, est1, C1617,M3,21.21);
+		Matricula *  m1 = new Matricula(act1, est1, C1920,M3,21.21);
 		REQUIRE(false==m1->aprobado());
 
 
@@ -290,7 +290,7 @@ TEST_CASE("Aprobados Suspenso", "[testAprobadoSuspenso]" ) {
 		REQUIRE(false==m1->aprobado());
 
 
-		Matricula *  m2 = new Matricula(act1, est1, C1617,M3,21.21);
+		Matricula *  m2 = new Matricula(act1, est1, C1920,M3,21.21);
 		REQUIRE(false==m2->aprobado());
 		m2->anyadirCalificacion(c);
 		REQUIRE(false==m2->aprobado());
@@ -298,7 +298,7 @@ TEST_CASE("Aprobados Suspenso", "[testAprobadoSuspenso]" ) {
 		m2->anyadirCalificacion(c2);
 		REQUIRE(true==m2->aprobado());
 
-		Matricula *  m3 = new Matricula(act1, est1, C1617,M3,21.21);
+		Matricula *  m3 = new Matricula(act1, est1, C1920,M3,21.21);
 		REQUIRE(false== m3->aprobado());
 		m3->anyadirCalificacion(c2);
 		REQUIRE(true==m3->aprobado());
